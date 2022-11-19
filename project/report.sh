084ad9a4dde75debc6ee7b4ec19cead026882c46
hellcatjack <hellcatjack@gmail.com>
2022-11-19 17:17:47 +0800

    Update report.sh

--- project/report.sh
+++ project/report.sh
@@ -3,6 +3,7 @@ server=`hostname`
 name=YourServerName
 ip=`curl -s ifconfig.me`
 reqServer=https://yourServer/req
+md5key=myKEY
 
 count=1
 for line in `find /etc/shadowsocks-libev -name *.json`
@@ -12,6 +13,7 @@ do
         port=`cat $line|grep port|awk -F ':' '{print $2}' |awk -F ',' '{print $1}'`
         url=`echo "$method:$pwd@$ip:$port"|base64 -w 0`
         url=`echo "ss://$( base64 <<< $method:$pwd@$ip:$port )#$name"|base64 -w 0`
-        echo `curl -s --data-urlencode "ps=$url" --data-urlencode "keyword=$server-$count" $reqServer`
+        md5=`echo -n $server-${count}$md5key$url|md5sum|cut -d ' ' -f1`
+	echo `curl -s --data-urlencode "ps=$url" --data-urlencode "keyword=$server-$count" --data-urlencode "md5=$md5"  $reqServer`
         count=$((${count} + 1))
 done
